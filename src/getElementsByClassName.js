6b80fa92c706063de577eb63f208c6cd2e4505a5
SterlingVix <amelocik@gmail.com>
2014-07-07 11:25:58 -0700

    getElementsByClass should be finished (though it's evaluating to false... I think this has to do with the stringify function being called, though). Stringify is in progress.

--- src/getElementsByClassName.js
+++ src/getElementsByClassName.js
@@ -5,5 +5,37 @@
 
 // But instead we're going to implement it from scratch:
 var getElementsByClassName = function(className){
-  // your code here
-};
+  var result = [];
+  var docLength = document.length;  //  var docLength = className.length;
+  var childSearchArray = [];
+  var nodeResults = [];
+  
+  // reject non-objects
+  if (typeof className !== "object" && typeof className !== "string") {
+    return className;
+  }
+  
+  var childSearch = function(node, target) {
+    var tempArray = []; // ???????
+    
+    for (var i = 0; i < node.length && node.length; i++) {  // node[i].childElementCount; // BASE CASE
+      if (node[i].className === target) { // node; node[i]; 
+        nodeResults.push(node[i]);
+        tempArray.push(node[i].outerHTML);
+        //debugger;
+      } // end BASE CASE -- if (push matching class innerHTML onto array)
+    
+    tempArray.push( childSearch(node[i].children, target) ); // recursive call to get next element
+    } // end for (iterate through childElements)
+
+  return tempArray;
+  }; // end childSearch
+  
+  childSearchArray = childSearch(document.children, className); // return is obsolete -- ignore
+  
+//   result = Array.prototype.slice.apply(nodeResults);
+//   debugger;
+  
+  return nodeResults;
+  
+}; // end getElementsByClassName
--- src/stringifyJSON.js
+++ src/stringifyJSON.js
@@ -3,5 +3,111 @@
 
 // but you don't so you're going to write it from scratch:
 var stringifyJSON = function(obj) {
-  // your code goes here
+  var thisChar, result;
+  var toDebug = false;
+  var oType = typeof obj;
+  var jObj = JSON.stringify(obj);
+  var objProps = [];
+  
+  var patt = new RegExp("[");
+  var res = patt.test(str);
+
+
+
+var string = eval(obj); // EVALUATES as an object literal...? Add escape characters...
+
+
+
+  getProps = function() {
+    for (var prop in obj) {
+      var tStr = "" + prop + ": " + obj[prop]; 
+      // console.log(tStr); 
+      objProps.push(tStr);
+    } };
+  getProps();
+  
+  // |||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||
+  // ||||||||||||||||     stringifyJSON myCode:     ||||||||||||||||
+  // |||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||
+                                  //dbg(obj, jObj); // for debugging
+  if (oType === "undefined") {
+    return "undefined";
+  } // end if (undefined)  
+  
+  if (oType === "boolean") {
+    return !!obj ? "true" : "false";
+  } // end if (boolean)
+    
+  if (oType === 'number') {
+    return ("" + obj);
+  } // end if (number)
+  
+  if (oType === 'string') {
+    result = "\"" + obj + "\"";
+    return result;
+  } // end if (number)
+  
+  if (oType === 'object') {
+    
+    if (obj === null) {
+      return "null";
+    }
+    if (Array.isArray(obj)) {
+      
+    } // end if (is array)
+    
+    
+    result = obj;
+    
+  } // end if (number)
+
+  
+  debugger;
+  
+  
+  
+  return result;
 };
+// |||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||
+// |||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||
+// |||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||
+
+// var stringifyJSON = JSON.stringify;
+    // console.log("DEBUG: " + obj + " is a number");
+
+// var jsst = JSON.stringify;
+// var dbg = function(input, input2) /*{if (toDebug) */ {console.log("DEBUG: " + input + " is a " + (typeof input) + "   ||   JSON: " + input2 + " is a " + (typeof input2));} //}
+
+// var num;
+// var jsNum;
+// var ary;
+// var jsAry;
+// var myObj;
+// var jsmyObj;
+
+////////////////////////////////////////////////////////////////////
+
+// var upJSON = function() {
+//   if (num) {jsNum = jsst(num);} else {console.log("No \'num\'");}
+//   if (ary) {jsAry = jsst(ary);} else {console.log("No \'jaAry\'");}
+//   if (myObj) {jsmyObj = jsst(myObj);} else {console.log("No \'myObj\'");}
+// }; // end upJSON
+
+// var testJSON = function() {
+//   if (typeof num !== 'undefined')   {if (num === jsNum)     {console.log("jsNum = jsst(num) = " + jsNum);}       else {if (typeof num !== 'undefined')   console.log("num != jsst(num) || " + typeof num + " !== " + typeof jsNum);} }
+//   if (typeof ary !== 'undefined')   {if (ary === jsAry)     {console.log("jsAry = jsst(ary) = " + jsAry);}       else {if (typeof ary !== 'undefined')   console.log("ary != jsst(ary) || " + typeof ary + " !== " + typeof jsAry);} }
+//   if (typeof myObj !== 'undefined') {if (myObj === jsmyObj) {console.log("jsmyObj = jsst(myObj) = " + jsmyObj);} else {if (typeof myObj !== 'undefined') console.log("myObj != jsst(myObj) || " + typeof myObj + " !== " + typeof jsmyObj);} }
+// }; // end upJSON
+
+// var upTest = function() {
+//   upJSON();
+//   console.log("------------");
+//   testJSON();
+//   console.log("||||||||||||");
+// };
+
+// var allProps = function(input) {
+//   for (var prop in input) {
+//     console.log("" + prop + ": " + input[prop]);
+//     }
+// };
